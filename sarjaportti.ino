ad1d07fb245836383b15351462a0c1bb221d3e3a
Pertti Heinola <pertti.heinola@ictpro.labra>
2018-11-07 13:58:00 +0200

    Pinnin vaihto pin12:sta pin3:een(PWM)

--- sarjaportti.ino
+++ sarjaportti.ino
@@ -1,11 +1,11 @@
 /*
  * Ohjelma vastaanottaa komentoja sarjaportilta(kirjaimia:'h','j','k','l')
- * 'h' polttaa lediä 1/4 teholla
- * 'j' polttaa lediä 2/4 teholla
- * 'k' polttaa lediä 3/4 teholla
- * 'l' polttaa lediä 4/4 teholla
+ * 'h' polttaa lediä 4/4 teholla
+ * 'j' polttaa lediä 3/4 teholla
+ * 'k' polttaa lediä 2/4 teholla
+ * 'l' polttaa lediä 1/4 teholla
  */
- #define LED 12
+ #define LED 3
  
 void setup() {
   
@@ -22,13 +22,13 @@ void loop() {
 
     int tieto = Serial.read();
     if(tieto == 'h'){
-      analogWrite(LED, 64);//polttaa lediä 1/4 teholla
+      analogWrite(LED, 64);//polttaa lediä 4/4 teholla
     }
     if(tieto == 'j'){
-      analogWrite(LED, 126);//polttaa lediä 1/4 teholla
+      analogWrite(LED, 126);//polttaa lediä 3/4 teholla
     }
     if(tieto == 'k'){
-      analogWrite(LED, 191);//polttaa lediä 1/4 teholla
+      analogWrite(LED, 191);//polttaa lediä 2/4 teholla
     }
     if(tieto == 'l'){
       analogWrite(LED, 255);//polttaa lediä 1/4 teholla
